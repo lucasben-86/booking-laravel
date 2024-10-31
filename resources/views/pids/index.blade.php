ad13681946c854d41660f159bafd5feae33ae598
panzazeta <76927307+panzazeta@users.noreply.github.com>
2024-10-30 22:47:33 -0300

    add code

--- resources/views/pids/index.blade.php
+++ resources/views/pids/index.blade.php
@@ -24,7 +24,7 @@
 
 <body>
 <div class="container mt-5">
-        <h2>Procesos disponibles</h2>
+        <h2>Procesos disponibles:</h2>
         <div class="btn-container mt-4">
             <a href="{{ url('/list') }}" class="btn btn-custom">Lista de trabajos</a>
             <a href="{{ url('/create') }}" class="btn btn-custom">Iniciar trabajo</a>
